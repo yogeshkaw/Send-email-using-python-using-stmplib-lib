652e32fc2324641026485ab2a9a79ece4c657df7
Yogesh kawadkar <40984491+yogeshkaw@users.noreply.github.com>
2019-02-27 16:50:38 +0530

    Create email.py

---
+++ email.py
@@ -0,0 +1,18 @@
+import smtplib
+import pandas as pd
+
+df = pd.read_csv("file:///Users/yogesh/Downloads/TechVita1.0_FinalSheet1.csv")#location of file in the form of csv
+
+mail = smtplib.SMTP('smtp.gmail.com', 587)//if you are using gmail account
+mail.ehlo() #helo can also be used
+mail.starttls()
+mail.login("unknown@gmail.com","Password")# type your email and password
+for i in range(34):
+    content ="Hello "+df['NAME'][i]+", "+"any message "#df['NAME'] is the name of column in the data frame or CSV file which contain name 
+    message='Subject: {}\n\n{}'.format(subject,content)
+    mail.sendmail("unknown@gmail.com",df['EMAIL'][i], message)#df['EMAIL'] is the name of column in the data frame or CSV file and which contain email
+    print(i)
+
+print("Completed")
+
+mail.close()    
